7d143019541409429260c98a5ddcf378a1dfc9e8
Andy Niccolai <andy.niccolai@gmail.com>
2012-01-26 17:38:51 -0700

    readme and default rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LdcmVideos::Application.config.secret_token = '00ade2a6871d1ee15f061f94c4a5d93273d9a96d8574070248f535aaf7486ca17c1cbe11a4ba349d9444605b467d96de63c823af1c582b2e868b1edb4fe303e3'
